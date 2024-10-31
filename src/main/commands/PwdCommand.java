9f6f681e23cea0ab623846f73bdda208132493a0
Youssef Moustafa <youssef.moustafa2012amer@gmail.com>
2024-10-31 19:50:20 +0200

    refactor pwd cmd

--- src/main/commands/PwdCommand.java
+++ src/main/commands/PwdCommand.java
@@ -1,10 +1,21 @@
 package main.commands;
+import main.executor.CommandExecutor;
 import main.fileSystem.*;
 public class PwdCommand implements Command {
     @Override
     public void execute(String[] args) {
+        CommandExecutor executor = new CommandExecutor();
         FileSystem fileSystem = FileSystem.getInstance();
         String currentDirectory = fileSystem.getCurrentDirectory().toString();
-        System.out.println(currentDirectory);
+        if (args == null || args.length == 0) {
+            System.out.println(currentDirectory);
+        } else {
+            if (executor.isChainedCmd(args[0])) {
+                executor.executeChainedCmd(args[0], args, currentDirectory.toString());
+            }
+            else {
+                System.out.println("Error: " + args[0] + " is not a command.");
+            }
+        }
     }
 }

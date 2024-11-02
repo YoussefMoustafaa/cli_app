9311c4e7dd0ce6b806270b27635c25b9abc60367
Youssef Moustafa <youssef.moustafa2012amer@gmail.com>
2024-11-02 16:51:50 +0200

    refactor mv cmd

--- src/main/commands/MvCommand.java
+++ src/main/commands/MvCommand.java
@@ -5,14 +5,29 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+import java.util.List;
+import main.fileSystem.*;
+
+import main.executor.CommandExecutor;
 
 public class MvCommand implements Command {
     @Override
     public void execute(String[] args) {
+        CommandExecutor executor = new CommandExecutor();
+        FileSystem fileSystem = FileSystem.getInstance();
         if (args.length > 2) {      // multiple files
+            // List<String> sourceFiles = new ArrayList<>();
+            // for (int i = 0; i < args.length; i++) {
+            //     sourceFiles.add(args[i]);
+            //     if (executor.isChainedCmd(args[i])) {
+            //         moveFiles(sourceFiles, args[i-1]);
+            //         return;
+            //     }
+            // }
 
             String destinationDirectory = args[args.length - 1];    // getting last arg which is the target dir
-            Path destinationDirectoryPath = Paths.get(destinationDirectory);
+            Path destinationDirectoryPath = Paths.get(fileSystem.getCurrentDirectory() + "/" + destinationDirectory);
             if (!isDirectory(destinationDirectoryPath)) {       // if arg is not a dir, stop execution
                 System.out.println("Error: target " + destinationDirectory + " is not a directory.");
                 return;
@@ -20,13 +35,13 @@ public class MvCommand implements Command {
 
 
             for (int i = 0; i < args.length-1; i++) {
-                Path sourceFilePath = Paths.get(args[i]);
+                Path sourceFilePath = Paths.get(fileSystem.getCurrentDirectory() + "/" + args[i]);
                 if (!Files.exists(sourceFilePath)) {      // check if any arg doesn't exist, stop execution
                     System.out.println("Error: " + args[i] + " does not exist.");
                     continue;
                 }
 
-                destinationDirectoryPath = Paths.get(destinationDirectory, args[i]);
+                destinationDirectoryPath = Paths.get(fileSystem.getCurrentDirectory() + "/" + destinationDirectory, args[i]);
                 try {
                     Files.move(sourceFilePath, destinationDirectoryPath, StandardCopyOption.REPLACE_EXISTING);
                     System.out.println("Moved \'" + sourceFilePath + "\' to " + destinationDirectory);
@@ -43,13 +58,13 @@ public class MvCommand implements Command {
             return;
         }
 
-        Path sourcePath = Paths.get(args[0]);
-        Path destinationPath = Paths.get(args[1]);      // for renaming
+        Path sourcePath = Paths.get(fileSystem.getCurrentDirectory() + "/" + args[0]);
+        Path destinationPath = Paths.get(fileSystem.getCurrentDirectory() + "/" + args[1]);      // for renaming
 
         String feedbackMessage = "Renamed";
 
         if (isDirectory(destinationPath)) {
-            destinationPath = Paths.get(args[1], args[0]);  // for moving
+            destinationPath = Paths.get(fileSystem.getCurrentDirectory() + "/" + args[1], args[0]);  // for moving
             feedbackMessage = "Moved";
         }
         
@@ -69,5 +84,17 @@ public class MvCommand implements Command {
     boolean isDirectory(Path path) {
         return Files.isDirectory(path);
     }
+
+
+    // void moveFiles(List<String> sourceFiles, String destinationDirectory) {
+    //     FileSystem fileSystem = FileSystem.getInstance();
+    //     Path destinationDirectoryPath = Paths.get(fileSystem.getCurrentDirectory() + "/" + destinationDirectory);
+    //     if (!isDirectory(destinationDirectoryPath)) {       // if arg is not a dir, stop execution
+    //         System.out.println("Error: target " + destinationDirectory + " is not a directory.");
+    //         return;
+    //     }
+    //     for (int i = 0; i < sourceFiles.size()-1; i++) {
+    //     }
+    // }
 
 }

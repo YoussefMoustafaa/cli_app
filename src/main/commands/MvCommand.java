0f7d2ec127e25f813b0f741d85267df5af56f156
Youssef Moustafa <youssef.moustafa2012amer@gmail.com>
2024-11-02 17:01:42 +0200

    refactor mv cmd part 2

--- src/main/commands/MvCommand.java
+++ src/main/commands/MvCommand.java
@@ -6,6 +6,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import main.fileSystem.*;
 
@@ -17,38 +18,18 @@ public class MvCommand implements Command {
         CommandExecutor executor = new CommandExecutor();
         FileSystem fileSystem = FileSystem.getInstance();
         if (args.length > 2) {      // multiple files
-            // List<String> sourceFiles = new ArrayList<>();
-            // for (int i = 0; i < args.length; i++) {
-            //     sourceFiles.add(args[i]);
-            //     if (executor.isChainedCmd(args[i])) {
-            //         moveFiles(sourceFiles, args[i-1]);
-            //         return;
-            //     }
-            // }
-
-            String destinationDirectory = args[args.length - 1];    // getting last arg which is the target dir
-            Path destinationDirectoryPath = Paths.get(fileSystem.getCurrentDirectory() + "/" + destinationDirectory);
-            if (!isDirectory(destinationDirectoryPath)) {       // if arg is not a dir, stop execution
-                System.out.println("Error: target " + destinationDirectory + " is not a directory.");
-                return;
-            }
-
-
-            for (int i = 0; i < args.length-1; i++) {
-                Path sourceFilePath = Paths.get(fileSystem.getCurrentDirectory() + "/" + args[i]);
-                if (!Files.exists(sourceFilePath)) {      // check if any arg doesn't exist, stop execution
-                    System.out.println("Error: " + args[i] + " does not exist.");
-                    continue;
+            List<String> sourceFiles = new ArrayList<>();
+            for (int i = 0; i < args.length; i++) {
+                if (executor.isChainedCmd(args[i])) {
+                    moveFiles(sourceFiles);
+                    sourceFiles.clear();
+                    String[] remArgs = Arrays.copyOfRange(args, i+1, args.length);
+                    executor.executeChainedCmd(args[i], remArgs, "");
+                    return;
                 }
-
-                destinationDirectoryPath = Paths.get(fileSystem.getCurrentDirectory() + "/" + destinationDirectory, args[i]);
-                try {
-                    Files.move(sourceFilePath, destinationDirectoryPath, StandardCopyOption.REPLACE_EXISTING);
-                    System.out.println("Moved \'" + sourceFilePath + "\' to " + destinationDirectory);
-                } catch (IOException e) {
-                    System.out.println("Error: unable to move " + args[i] + " to " + destinationDirectory);
+                else {
+                    sourceFiles.add(args[i]);
                 }
-
             }
             return;
         }
@@ -86,15 +67,31 @@ public class MvCommand implements Command {
     }
 
 
-    // void moveFiles(List<String> sourceFiles, String destinationDirectory) {
-    //     FileSystem fileSystem = FileSystem.getInstance();
-    //     Path destinationDirectoryPath = Paths.get(fileSystem.getCurrentDirectory() + "/" + destinationDirectory);
-    //     if (!isDirectory(destinationDirectoryPath)) {       // if arg is not a dir, stop execution
-    //         System.out.println("Error: target " + destinationDirectory + " is not a directory.");
-    //         return;
-    //     }
-    //     for (int i = 0; i < sourceFiles.size()-1; i++) {
-    //     }
-    // }
+    void moveFiles(List<String> sourceFiles) {
+        FileSystem fileSystem = FileSystem.getInstance();
+        String destinationDirectory = sourceFiles.get(sourceFiles.size()-1);  // last arg is the target dir
+        Path destinationDirectoryPath = Paths.get(fileSystem.getCurrentDirectory() + "/" + destinationDirectory);
+
+        if (!isDirectory(destinationDirectoryPath)) {       // if arg is not a dir, stop execution
+            System.out.println("Error: target " + destinationDirectory + " is not a directory.");
+            return;
+        }
+        
+        for (int i = 0; i < sourceFiles.size()-1; i++) {
+            Path sourceFilePath = Paths.get(fileSystem.getCurrentDirectory() + "/" + sourceFiles.get(i));
+            if (!Files.exists(sourceFilePath)) {      // check if any arg doesn't exist, stop execution
+                System.out.println("Error: " + sourceFiles.get(i) + " does not exist.");
+                continue;
+            }
+
+            destinationDirectoryPath = Paths.get(fileSystem.getCurrentDirectory() + "/" + destinationDirectory, sourceFiles.get(i));
+            try {
+                Files.move(sourceFilePath, destinationDirectoryPath, StandardCopyOption.REPLACE_EXISTING);
+                System.out.println("Moved \'" + sourceFilePath + "\' to " + destinationDirectory);
+            } catch (IOException e) {
+                System.out.println("Error: unable to move " + sourceFiles.get(i) + " to " + destinationDirectory);
+            }
+        }
+    }
 
 }
